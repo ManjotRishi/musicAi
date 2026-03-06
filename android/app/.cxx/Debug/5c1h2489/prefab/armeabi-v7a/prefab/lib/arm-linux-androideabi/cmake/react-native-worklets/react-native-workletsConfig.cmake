c036af8f577e4daab7f1ebc6c900347187e4e333
ManjotRishi <manjotrishi001@gmail.com>
2026-03-06 13:36:49 +0530

    changes

---
+++ android/app/.cxx/Debug/5c1h2489/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-worklets/react-native-workletsConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-worklets::worklets)
+add_library(react-native-worklets::worklets SHARED IMPORTED)
+set_target_properties(react-native-worklets::worklets PROPERTIES
+    IMPORTED_LOCATION "/Users/techcapanicus/Documents/Web-Projects/MusicAI/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/201l4k5r/obj/armeabi-v7a/libworklets.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/techcapanicus/Documents/Web-Projects/MusicAI/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
